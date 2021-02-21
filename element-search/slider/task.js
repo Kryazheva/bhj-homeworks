e9c6da9fe165d3b729754168b4c6f0d841d074a7
kristina <kistina@localhost>
2021-02-21 18:34:02 +0300

    slide_bar
    add findIndex((value) => value.classList.contains('slider__item_active'))

---
+++ element-search/slider/task.js
@@ -0,0 +1,26 @@
+'use srtict';
+const prev = document.querySelector('.slider__arrow_prev');
+const next = document.querySelector('.slider__arrow_next');
+let arrSelector = Array.from(document.getElementsByClassName('slider__item'));
+let active = arrSelector.findIndex((value) => value.classList.contains('slider__item_active'));
+
+next.onclick = () => {
+    if (active > arrSelector.length - 1) {
+        active = 0;
+        arrSelector[active].classList.add('slider__item_active');
+    }
+        arrSelector[active].classList.remove('slider__item_active');
+        active = active + 1;
+        arrSelector[active].classList.add('slider__item_active'); 
+}
+
+prev.onclick = () => {
+    if (active == -1) {
+        active = arrSelector.length - 1;
+    }
+    arrSelector[active].classList.remove('slider__item_active');
+    active = active - 1;
+    arrSelector[active].classList.add('slider__item_active');
+    
+    
+}
