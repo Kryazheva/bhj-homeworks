4de3e75c3624742d4e3ddf594b668ef30ea7fa2c
kristina <kistina@localhost>
2021-03-06 21:48:56 +0300

    book-reader
    make toggle class

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -2,40 +2,49 @@ const classFontSize = Array.from(document.getElementsByClassName('font-size'));
 const classContent = document.querySelector('.book__content');
 
 classFontSize.forEach (element => {
-    element.addEventListener('click', function(evt) {
-        evt.preventDefault();
-        if (element.classList.contains('font-size_active')) {
-            removeClassOfTabs(classFontSize, element.closest('book__control'));
-            toggleText(classFontSize);
-        } else {
-            removeClassOfTabs(classFontSize, element.closest('book__control'));
+    element.addEventListener('click', function(e) {
+        e.preventDefault();
+        delClass(classFontSize);
+        delClassFontSize(classContent);
+        toggleFontSize(classFontSize);   
+        // console.log(e.target);
+        // console.log(element.dataset.size);
+        if (element.dataset.size === 'small' || element.dataset.size === 'big') {
             element.classList.add('font-size_active');
-            toggleText(classFontSize);
+        } else {
+            element.classList.add('font-size_active');
         }
     })
-})
+});
 
-function removeClassOfTabs (arr, tabsActive) {
-    arr.forEach(element => {
-        if (tabsActive === element.closest('book__control')) {
-           element.classList.remove('font-size_active');
-        }
-    });
- }  
+let delClass = (arr) => {
+    arr.forEach((element) => {
+      if (element.classList.contains('font-size_active')) {
+          element.classList.remove('font-size_active');
+        //   console.log(element)
+       }
+    })
+};
 
-function toggleText (arr) {
-    arr.forEach(element => {
-        if (element.dataset.size === 'small') {
-            // console.log(element.dataset.size)
-            classContent.classList.add('book_fs-small');
-         } else if (element.dataset.size === 'big') {
-            // console.log(element.dataset.size)
-            classContent.classList.remove('book_fs-small');
-            classContent.classList.add('book_fs-big');
-         } else {
-            classContent.classList.remove('book_fs-small');
-            classContent.classList.remove('book_fs-big');
+let delClassFontSize = (element) => {
+    if (element.classList.contains('book_fs-big')) {
+        element.classList.remove('book_fs-big');
+    } else if (element.classList.contains('book_fs-small')) {
+        element.classList.remove('book_fs-small');
+    } else {
+        element.classList.remove('book_fs-small');
+        element.classList.remove('book_fs-big');
+    }
+};
+
+let toggleFontSize = (arr) => {
+    arr.forEach((element) => {
+        if (element.classList.contains('font-size_small')) {
+          classContent.classList.add('book_fs-small');
+        } else if (element.classList.contains('font-size_big')) {
+          classContent.classList.add('font-size_big');
+        } else {
+          '';
         }
     })
-
-}
+};
