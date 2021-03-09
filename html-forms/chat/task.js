40c9955e4f21d9b7fa49c15cb71e3a268fed4439
kristina <kistina@localhost>
2021-03-10 02:48:04 +0300

    chat
    добавлен шаблон template

---
+++ html-forms/chat/task.js
@@ -0,0 +1,57 @@
+const buttonChat = document.querySelector('.chat-widget__side');
+const chat = document.querySelector('.chat-widget');
+const input = document.getElementById('chat-widget__input');
+const conteiner = document.getElementById('chat-widget__messages');
+const template = document.querySelector('template').content;
+const message = template.querySelector('.message');
+const arrBotMessage = ['Добрый день!', 'Чем могу помочь', 'Мы вам перезвоним'];
+// console.log(openChat)
+// input.onfocus = (e) => {
+//     console.log(e.target)
+// };
+let setLength = (i) => {
+    if (i.value.length > 3) {
+        console.log(i.value);
+        return false;
+    };
+};
+
+let postBotMessage = (element, arr, cont) => {
+    //   console.log(element.cloneNode(true))
+    const arrTemplate = element.cloneNode(true);
+    const time = arrTemplate.querySelector('.message__time');
+    const text = arrTemplate.querySelector('.message__text');
+    time.textContent = new Date().toTimeString();
+    text.textContent = arr[Math.floor(Math.random() * arr.length)];
+    cont.appendChild(arrTemplate);
+};
+    
+let postClientMessage = (element, input, cont) => {
+    const arrTemplate = element.cloneNode(true);
+    const time = arrTemplate.querySelector('.message__time');
+    const text = arrTemplate.querySelector('.message__text');
+    time.textContent = new Date().toTimeString();
+    text.textContent = input.value;
+    // console.log(input.value)
+    cont.appendChild(arrTemplate);
+};
+
+buttonChat.addEventListener('click', () => {
+    chat.classList.add('chat-widget_active');
+});
+
+input.addEventListener('keydown', (e) => {
+    setLength(input);
+    if (e.code === 'Enter') {
+     postClientMessage (message, input, conteiner);
+     setTimeout(() => {
+        postBotMessage(message, arrBotMessage, conteiner);
+     }, 1000);
+     input.value = '';   
+    }   
+});
+
+
+
+
+
