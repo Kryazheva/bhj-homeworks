9330cfe4830c43c29f559edf4c77329475b99bc9
kristina <kistina@localhost>
2021-03-13 18:27:41 +0300

    делегирование

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -2,10 +2,16 @@
 const arrLi = document.querySelector('.interest');
 const arrInput = Array.from(document.getElementsByClassName('interest__check'));
 
-arrInput.forEach((input) => {
-    input.addEventListener('change', (e) => {
-        if (e.target.closest('li.interest')) {
-            console.log('y')
-        } 
-    })
+// arrInput.forEach((input) => {
+//     input.addEventListener('change', (e) => {
+//         if (e.target.closest('li.interest')) {
+//             console.log('y')
+//         } 
+//     })
+// })
+
+document.querySelector('.interests_main').addEventListener('change', e => {
+    if (e.target.className === 'interest__check') {
+        console.log('yes')
+    }
 })
